0de17810efdfb9b081b4639c7f242742ee52a380
devesh05 <35624629+devesh05@users.noreply.github.com>
2018-01-22 21:50:37 +0530

    Create devesh3.c

---
+++ devesh3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{ char r;
+printf("enter a character");
+scanf("%c",&r);
+if(r=='a'||r=='e'||r=='i'||r=='o'||r=='u')
+  {
+  printf("vowels");
+  }
+else
+ {
+ printf("contants");
+ }
+
+}
